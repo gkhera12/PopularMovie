e813ff25a12f4a581d3d5830473d0c047d788da8
Gaurav <gkhera12@gmail.com>
2016-03-21 21:32:52 +1100

    Refactoring

--- app/src/main/java/com/example/eightleaves/popularmovie/FetchReviewsTask.java
+++
@@ -1,7 +0,0 @@
-package com.example.eightleaves.popularmovie;
-
-/**
- * Created by gkhera on 11/03/2016.
- */
-public class FetchReviewsTask {
-}
--- app/src/main/java/com/example/eightleaves/popularmovie/MainActivity.java
+++ app/src/main/java/com/example/eightleaves/popularmovie/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.facebook.stetho.Stetho;
+//import com.facebook.stetho.Stetho;
 
 public class MainActivity extends AppCompatActivity {
     private String mSortSetting;
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Stetho.initializeWithDefaults(this);
+  //      Stetho.initializeWithDefaults(this);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
